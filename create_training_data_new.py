efb8c8a76e8c3b95f4b8ccd062d7490ca1478ae4
qwertz12345 <mivogelpohl@gmail.com>
2018-06-13 21:29:46 +0200

    Implemented multiprocessing for saving the images.

--- create_training_data_new.py
+++ create_training_data_new.py
@@ -1,10 +1,11 @@
 import pathlib
 import time
+from multiprocessing import Pool
 
 import numpy as np
 from PIL import Image
 
-import grabscreen
+from grabscreen import grab_screen
 from getkeys import key_check
 
 
@@ -31,31 +32,40 @@ def pause(time_adjustment):
     return time_adjustment + pause_stop_time - pause_start_time, stop
 
 
+def save_image(name, image):
+    im = Image.fromarray(image[..., ::-1])
+    im.save(name)
+
+
 def save(training_data, run_name):
     """
     Saves images as png, key and time data as npy.
-    Reduce data size by resizing and compressing (losslessly) the images before saving.
+    Reduce data size by resizing and compressing (lossless) the images before saving.
     :param training_data: Tuple of the image, keys and time stamps
-    :param run_name: usually the track name
+    :param run_name: Usually the track name.
     """
-    #                                           convert BGR -> RGB
-    screenshots = [Image.fromarray(elem[0][..., ::-1])  # .resize((400, 215), resample=Image.HAMMING)
-                   for elem in training_data]
+
     keys_and_times = np.array([(elem[1], elem[2]) for elem in training_data])
     time_diffs = np.diff(np.concatenate((np.array([0.0]), keys_and_times[:, 1])))
-
     training_directory = r"E:\Trackmania Data\training_data_new\\" + run_name + "_" + str(
         time.strftime("%Y%m%d-%H%M%S"))
     pathlib.Path(training_directory + r"\\screenshots").mkdir(parents=True)
     with open(training_directory + r"\\" + "keys_timings.txt", "w") as text_file:
         for k, line in enumerate(keys_and_times):
             text_file.write("{0:5}     {1:20s}     {2:f}       {3:f}\n".format(k, str(line[0]), line[1], time_diffs[k]))
-    for k, im in enumerate(screenshots):
-        im.save(training_directory + r"\\screenshots\\" + str(k) + "_" + str(keys_and_times[k][0]) + ".png")
+
+    image_names = []
+    for k, _ in enumerate(training_data):
+        image_names.append(training_directory + r"\\screenshots\\" + str(k) + "_" + str(keys_and_times[k][0]) + ".png")
+
+    pool = Pool()
+    pool.starmap(save_image, [(image_names[k], e[0]) for k, e in enumerate(training_data)])
+    pool.close()
+    pool.join()
 
 
 def main(file_name):
-    screen = grabscreen.grab_screen(region=(8, 200, 800, 430), window_title="TrackMania Nations Forever")
+    screen = grab_screen(region=(8, 200, 800, 430), window_title="TrackMania Nations Forever")
     training_data = []
     _ = key_check()
     time_adjustment, stop_signal = pause(time.time())
@@ -75,7 +85,8 @@ def main(file_name):
 
 
 if __name__ == '__main__':
+    save_name = input("Enter run name: ")
     print("Keys: 't' to pause \n"
           "      'r' to resume \n"
           "      'o' and 'p' simultaneously while paused to save and exit.")
-    main(input("Enter run name: "))
+    main(save_name)
